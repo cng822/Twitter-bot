e8cc7fafadaff8d12adc377c8dfb157736a9f867
Carrie <cng822@aucklanduni.ac.nz>
2021-02-03 23:12:32 +1300

    added reading and writing tweet

--- twitter.py
+++ twitter.py
@@ -1,27 +1,51 @@
 import tweepy
 import time 
 
-auth = tweepy.OAuthHandler('Xk4cLGPacZqeR6MYzMYwkJW7x','cVO9ZRhdCdU59GlmgjzJokLMoies5ac6rXmAgMlM2AVSQu92ix')
-auth.set_access_token('1885956528-6VMkXQWHzF5fZWnEFLDjQCZ5BK9QNaGmMDzvRq8','1fTu5byWco023yUPtXkGbfW8oR3dHBndE5f859wVWiO5U')
+auth = tweepy.OAuthHandler('XX','YY') # access code unique to your account 
+auth.set_access_token('ZZ','AA')
 
 api = tweepy.API(auth, wait_on_rate_limit =  True, wait_on_rate_limit_notify = True)
 user = api.me()
 
-search = 'WayV'
-numberOfTweets = 5 ## limit fo 500 
+FILE_NAME = 'post.txt'
 
-for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
-    try: 
-        print('Tweet Liked')
-        tweet.favorite()
-        tweet.retweet()
-        api.update_status('WayV Liked')
-        time.sleep(20)
-        # tweet.destroy_favorite()
-    except tweepy.TweepError as e: 
-        print(e.reason)
-    except StopIteration:
-        break
+def read_last_seen(FILE_NAME):
+        file_read = open(FILE_NAME, 'r')
+        last_seen_id = int(file_read.read().strip())
+        file_read.close()
+        return last_seen_id
+
+def store_last_seen(FILE_NAME):
+    file_write = open(FILE_NAME, 'w')
+    file_write.write(str(last_seen_id))
+    file_write.close()
+    return 
+
+tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode = 'extended')
+
+for tweet in tweets: 
+    
+    if '#ultimatebot' in tweet.full_text.lower():
+        print(str(tweet.id) + ' - ' + tweet.full_text)
+        api.update_status("@" + tweet.user.screen_name + " Auto reply works :)", tweet_id)
+        store_last_seen(FILE_NAME, tweet.id) 
+
+# search = 'WayV'
+# numberOfTweets = 5 ## limit fo 500 
+# count = 0 
+# for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
+#     try: 
+#         print('Tweet Liked')
+#         tweet.favorite()
+#         tweet.retweet()
+#         count = count + 1
+#         api.update_status('WayV Tweet Liked. User:' + tweet.user.screen_name)
+#         time.sleep(2)
+#     except tweepy.TweepError as e: 
+#         print(e.reason)
+#     except StopIteration:
+#         break
+
 
 
 # for follower in tweepy.Cursor(api.followers).items():  ## to find name of followers 
